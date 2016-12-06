ccf62df698ac88ce36460690126771661d6f7014
samuel.mortenson <samuel.mortenson@2582268.no-reply.drupal.org>
2016-12-06 14:05:14 -0500

    Issue #2693163 by samuel.mortenson: Quick Edit support for fields displayed using the ctools_field block.

--- src/PanelizerEntityViewBuilder.php
+++ src/PanelizerEntityViewBuilder.php
@@ -353,6 +353,13 @@ class PanelizerEntityViewBuilder implements EntityViewBuilderInterface, EntityHa
     foreach ($entities as $id => $entity) {
       $panels_display = $this->panelizer->getPanelsDisplay($entity, $view_mode, $displays[$entity->bundle()]);
       $build[$id] = $this->buildPanelized($entity, $panels_display, $view_mode, $langcode);
+
+      // Allow modules to modify the render array.
+      $alter_types = array(
+        "{$this->entityTypeId}_view",
+        'entity_view',
+      );
+      $this->moduleHandler->alter($alter_types, $build[$id], $entity, $displays[$entity->bundle()]);
     }
 
     return $build;
